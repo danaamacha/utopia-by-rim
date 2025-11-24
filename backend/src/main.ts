b2218c8cc620c4231c82ec24c5318e083d9fda90
Dana Amasha <danaamacha02@gmail.com>
2025-11-24 17:00:47 +0200

    Add custom domain to CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173','https://utopiabyrim.com','https://www.utopiabyrim.com'],
     credentials: true,
   });
 
